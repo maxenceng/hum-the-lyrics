b2c70da7ab540b43ddecf3b09cbd05bf544bf68f
Maxence GROSJEAN <maxence.grosjean@lab-solutec.fr>
2019-04-19 17:47:27 +0200

    Modified the script so that we could give a range of lyrics to add in the DB, should probably automatise it

--- back-end/src/scripts/genius.js
+++ back-end/src/scripts/genius.js
@@ -5,6 +5,10 @@ const mongoose = require('mongoose')
 const lyricist = new Lyricist(process.env.CLIENT_ACCESS_TOKEN)
 
 mongoose.connect('mongodb://localhost/hum', { useNewUrlParser: true })
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+mongoose.connection.once('open', function() {
+  console.log('Connected!')
+});
 
 const lyricsSchema = new mongoose.Schema({
   artist: String,
@@ -21,8 +25,28 @@ const addSong = async (songID) => {
     title: song.title,
     artist: song.primary_artist.name,
   })
-  lyrics.save()
-  process.exit()
+  await lyrics.save()
+  return song
 }
 
-addSong(714198)
+const floor = Number.parseInt(process.argv[2], 10)
+const ceiling = Number.parseInt(process.argv[3], 10)
+
+if (process.argv.length <= 2) {
+  console.log('Not enough parameters')
+  process.exit(0)
+}
+
+(async () => {
+  // body of the function
+  for (let i = floor; i <= ceiling; i++) {
+    try {
+      const song = await addSong(i)
+      console.log(`Added song ${i} : ${song.title} - ${song.primary_artist.name}`)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+  console.log('For loop finished')
+  process.exit(0)
+})()
